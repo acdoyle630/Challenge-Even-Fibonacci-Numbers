9427ea5ee9e5bee71b96d204ca7998a68c0fc75c
Adam <acdoyle630@gmail.com>
2017-03-15 09:59:17 -1000

    fib

--- even_fib_nums.js
+++ even_fib_nums.js
@@ -6,24 +6,52 @@
  * @return {Number} sum
  */
 function _sumFibs( maxFibValue ) {
+  var current = 2;
+  var previous = 1;
   var sum = 0;
-
-  // do your work here
-
-  return sum;
+  var fibArr = [1, 2];
+  var result = 0;
+  for (var i=1; i<maxFibValue; i++) {
+  	sum = current + previous;	
+  	previous = current;			
+  	current = sum;	
+  	fibArr.push(sum);	
+  	if(fibArr[i] % 2 === 0 && fibArr[i] <= maxFibValue){
+  		result+=fibArr[i];
+  	}		
 }
+	
+	
+
+  return result;
+}
+_sumFibs(4000000);
 
 // bonus round
 function _highestFibonacciNumber (maxFibValue){
   var highest = 0;
+  var current = 2;
+  var previous = 1;
+  var counter = 0;
+  var fibArr = [1,2];
+  for (var i=1; i<maxFibValue; i++) {
+  	counter = current + previous;
+  	previous = current;
+  	current = counter;
+  	if(counter <= maxFibValue){
+  	fibArr.push(counter);
+  	highest = fibArr[fibArr.length-1];
+  }
+  
+  }
+  return highest;
+}
 
   //define your base case, validate your input
 
-
+  
   //do your work here
-
-  return highest;
-};
+  console.log(_highestFibonacciNumber(100));
 
 /**
  * Do not modify code below.
